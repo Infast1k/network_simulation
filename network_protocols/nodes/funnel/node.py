c18145e4a410b062787cf63618fe7c1111fdd235
Mihail <infast1k@mail.ru>
2024-10-28 14:21:44 +0300

    feat(nodes): implement find neighbors method

--- network_protocols/nodes/funnel/node.py
+++ network_protocols/nodes/funnel/node.py
@@ -2,6 +2,21 @@ from network_protocols.nodes.base import BaseFunnelNode, BaseNodeProps
 
 
 class FunnelNode(BaseFunnelNode):
-    # TODO: rework this method
     def find_neighbors(self, nodes: list[BaseNodeProps]) -> None:
-        return super().find_neighbors(nodes)
+        """
+        Finds the neighbors of the current node.
+        Before finding neighbors, it clears the list of neighbors.
+        """
+        if len(self._neighbors) > 0:
+            self._neighbors.clear()
+
+        center_x, center_y = self.coordinates
+
+        for neighbor in nodes:
+            if neighbor.oid == self.oid:
+                continue
+
+            x, y = neighbor.coordinates
+
+            if (x - center_x) ** 2 + (y - center_y) ** 2 <= self._radius ** 2:
+                self._neighbors.append(neighbor)
